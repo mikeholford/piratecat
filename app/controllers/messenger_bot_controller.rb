6a6a1cfc59d3314d851dd28ccb104b522b4fcbb6
Mike Holford <mike.holford@redant.com.au>
2016-04-22 16:33:37 +1000

    updated messenger bot controller actions

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -3,13 +3,16 @@ class MessengerBotController < ApplicationController
   def message(event, sender)
     # profile = sender.get_profile
     sender.reply({ text: "Reply: #{event['message']['text']}" })
+    puts 'message message message!!!!!'
   end
 
   def delivery(event, sender)
+    puts 'delivery delivery delivery!!!!!'
     #BlahBlah
   end
 
   def postback(event, sender)
+    puts 'postback postback postback!!!!!'
     #BlahBlah
   end
 
