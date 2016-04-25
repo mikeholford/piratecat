c12acd295c88871023464a00e6a9952955412a44
Mike Holford <mike.holford@redant.com.au>
2016-04-26 09:53:14 +1000

    post webhookd response edited

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -3,7 +3,6 @@ class BotController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def webhook
-    Rails.logger.debug "Webhook working bitches!"
    if params['hub.verify_token'] == "pirate_cat_verify"
      render text: params['hub.challenge'] and return
    else
@@ -12,8 +11,7 @@ class BotController < ApplicationController
   end
 
   def receive_message
+    puts "Message recieved"
     Rails.logger.debug params.inspect
-    puts "HAHA THIS IS ACTUALLY WORKING"
-    Rails.logger.debug "TESTING 123piratecat"
   end
 end
