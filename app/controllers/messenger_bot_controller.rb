764872693c867a0354c63be480a58e1aed2f692c
Mike Holford <mike.holford@redant.com.au>
2016-04-26 10:20:36 +1000

    update messgae

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,5 +1,7 @@
 class MessengerBotController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+
   def message(event, sender)
     # profile = sender.get_profile
     sender.reply({ text: "Reply: #{event['message']['text']}" })
@@ -14,6 +16,33 @@ class MessengerBotController < ApplicationController
   def postback(event, sender)
     puts 'postback postback postback!!!!!'
     #BlahBlah
+  end
+
+
+
+
+  def webhook
+   if params['hub.verify_token'] == "pirate_cat_verify"
+     render text: params['hub.challenge'] and return
+   else
+     render text: 'error' and return
+   end
+  end
+
+  def receive_message
+    Rails.logger.debug params.inspect
+
+     if params[:entry]
+       messaging_events = params[:entry][0][:messaging]
+         messaging_events.each do |event|
+         sender = event[:sender][:id]
+         if (text = event[:message] && event[:message][:text])
+          puts "WOOP Message recieved"
+           # send_text_message(sender, “Hi there! You said: #{text}. The Bots”)
+         end
+       end
+     end
+     render nothing: true
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'statics#landing'
 
-  # get 'bot/webhook' => 'bot#webhook'
-  # post 'bot/webhook' => 'bot#receive_message'
+  get '/webhook' => 'messenger_bot#webhook'
+  post '/webhook' => 'messenger_bot#receive_message'
 
 
-  mount Messenger::Bot::Space => "/webhook"
+  # mount Messenger::Bot::Space => "/webhook"
 
 
 
