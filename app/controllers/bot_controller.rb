7df479a3d4554a286fd1933822b02cf7bf21e749
Mike Holford <mike.holford@redant.com.au>
2016-05-09 14:36:48 +1000

    add new receipt

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -29,7 +29,7 @@ class BotController < ApplicationController
             'joke' => 'joke_trigger',
             'meow' => 'joke_trigger',
             'help' => 'help_trigger',
-            'receipt' => 'help_trigger'
+            'receipt' => 'receipt_trigger'
           }
 
           trigger_match = false
@@ -187,34 +187,42 @@ class BotController < ApplicationController
       recipient: {
         id: sender
       },
-      message: {
+      message:{
         attachment:{
           type: "template",
-          payload: {
+          payload:{
             template_type: "receipt",
-            recipient_name: "Mike Holford",
+            recipient_name: "Stephane Crozatier",
             order_number: "12345678902",
-            currency: "AUD",
+            currency: "USD",
             payment_method: "Visa 2345",
             order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
             timestamp: "1428444852",
-            elements: [
+            elements:[
               {
-                title: "Pirate Cat T-Shirt",
-                subtitle: "100% Soft and Luxurious Kitten",
+                title: "Classic White T-Shirt",
+                subtitle: "100% Soft and Luxurious Cotton",
                 quantity: 2,
                 price: 50,
-                currency: "AUD",
+                currency: "USD",
                 image_url: "http://petersapparel.parseapp.com/img/whiteshirt.png"
+              },
+              {
+                title: "Classic Gray T-Shirt",
+                subtitle: "100% Soft and Luxurious Cotton",
+                quantity: 1,
+                price: 25,
+                currency: "USD",
+                image_url: "http://petersapparel.parseapp.com/img/grayshirt.png"
               }
             ],
             address:{
-              street_1: "5/142 Pittwater Road",
+              street_1: "1 Hacker Way",
               street_2: "",
-              city: "Sydney",
-              postal_code: "2095",
-              state: "NSW",
-              country: "AU"
+              city: "Menlo Park",
+              postal_code: "94025",
+              state: "CA",
+              country: "US"
             },
             summary:{
               subtotal: 75.00,
