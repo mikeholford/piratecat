cee0de33f61b6513a97a1525b24a3035081a6d22
Mike Holford <mike.holford@redant.com.au>
2016-05-11 12:32:50 +1000

    user_details testing

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -103,7 +103,7 @@ class BotController < ApplicationController
           type: "template",
           payload: {
             template_type: "button",
-            text: "👋 Hello!\n\nWelcome to the Pirate Cat #{user_details['first_name']}! Select an option below to get started. Or type MEOW 🐱 to hear a funny cat joke.",
+            text: "👋 Hello!\n\nWelcome to the Pirate Cat #{user_details["first_name"]} or #{user_details.first}! Select an option below to get started. Or type MEOW 🐱 to hear a funny cat joke.",
             buttons: [
               {
                 type: "postback",
